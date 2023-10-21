d761442fdbb1e6e696866c52754d0c8afec546ff
JiseokLee <jisuk500@gmail.com>
2023-10-22 00:09:01 +0900

    add avatar

--- how_i_generate_this.sh
+++ how_i_generate_this.sh
@@ -4,3 +4,6 @@ sudo gem install jekyll bundler
 # https://jjikin.com/posts/Jekyll-Chirpy-%ED%85%8C%EB%A7%88%EB%A5%BC-%ED%99%9C%EC%9A%A9%ED%95%9C-Github-%EB%B8%94%EB%A1%9C%EA%B7%B8-%EB%A7%8C%EB%93%A4%EA%B8%B0(2023-6%EC%9B%94-%EA%B8%B0%EC%A4%80)/
 
 # installing nodejs 20.x at here, https://github.com/nodesource/distributions
+
+
+## 세부 세팅 https://www.irgroup.org/posts/jekyll-chirpy/
